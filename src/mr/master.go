076b2965f6844660034dddd5c083f863736b65f6
WenheLi <wl1508@nyu.edu>
2020-06-09 16:45:26 +0800

    feat: Add init start for lab1

--- src/mr/master.go
+++ src/mr/master.go
@@ -1,15 +1,41 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
 
+type TaskStatus = int
+
+const (
+	IDLE TaskStatus = iota
+	PROGRESS
+	COMPLETE
+)
+
+type TaskType = int
+
+const (
+	MAP    TaskType = 0
+	REDUCE          = 1
+)
+
+type Task struct {
+	taskType   TaskType
+	taskStatus TaskStatus
+	id         int
+	fileName   string
+}
 
 type Master struct {
 	// Your definitions here.
-
+	files   []string
+	taskCh  chan Task
+	done    bool
+	nReduce int
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -23,7 +49,6 @@ func (m *Master) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
-
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -50,8 +75,14 @@ func (m *Master) Done() bool {
 
 	// Your code here.
 
-
 	return ret
+}
+
+func __max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
 }
 
 //
@@ -61,9 +92,12 @@ func (m *Master) Done() bool {
 //
 func MakeMaster(files []string, nReduce int) *Master {
 	m := Master{}
+	m.files = files
+	m.nReduce = nReduce
+
+	m.taskCh = make(chan Task, __max(nReduce, len(files)))
 
 	// Your code here.
-
 
 	m.server()
 	return &m
